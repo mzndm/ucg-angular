d6202c6c2eaeffad25192d57048b32e42d350a51
Dmytro Mazun <dimamazun1@gmail.com>
2023-10-02 01:45:25 +0300

    delete directive test file

--- src/app/features/users/directives/password-visibility.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordVisibilityDirective } from './password-visibility.directive';
-
-describe('PasswordVisibilityDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PasswordVisibilityDirective();
-    expect(directive).toBeTruthy();
-  });
-});
